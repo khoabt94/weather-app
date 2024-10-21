33b714b15de753573dc8597c85f3257b316d7100
khoabt94 <khoa.bui@buymed.com>
2024-10-21 18:27:34 +0700

    fix: minor bug

--- src/components/search-results/search-history/index.tsx
+++ src/components/search-results/search-history/index.tsx
@@ -13,16 +13,19 @@ export default function SearchHistory() {
   return (
     <section className="bg-white/15 dark:bg-dark/15 w-full mt-6 rounded-[24px] relative px-5 py-6 shadow">
       <h3 className="text-gray-900 text-base dark:text-white">Search History</h3>
-      <div className="space-y-4 mt-4 h-[300px] overflow-y-auto pr-2">
-        {histories.map((history) => (
-          <HistoryItem
-            history={history}
-            key={history.id}
-            handleDelete={deleteHistory}
-            handleClickSearch={handleClickSearch}
-          />
-        ))}
-      </div>
+      {histories.length ? (
+        <div className="space-y-4 mt-4 h-[300px] overflow-y-auto pr-2">
+          {histories.map((history) => (
+            <HistoryItem
+              history={history}
+              key={history.id}
+              handleDelete={deleteHistory}
+              handleClickSearch={handleClickSearch}
+            />
+          ))}
+        </div>
+      ) : (<p className='italic text-sm text-center mt-4'>Try to search new one!</p>)}
+
     </section>
   )
 }
